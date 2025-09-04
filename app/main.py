3aface2b4f8b20c23b1870da7f2fe181500eb35f
Stefan Drobić <stefan.drobic@vizlore.com>
2025-09-04 20:41:26 +0200

    removed old frontend static deployment files from this repo, they're getting handled via the dashboard not'

--- app/main.py
+++ app/main.py
@@ -4,7 +4,6 @@ from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from starlette.exceptions import HTTPException as StarletteHTTPException, HTTPException
 
 from api.api_v1.api import api_router
 
@@ -12,10 +11,6 @@ from core.config import settings
 from init.init_gatekeeper import register_apis_to_gatekeeper
 
 from jobs.background_tasks import get_weather_data
-
-from fastapi.staticfiles import StaticFiles
-
-import os
 
 @asynccontextmanager
 async def lifespan(fa: FastAPI):
@@ -48,19 +43,3 @@ if settings.CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix="/api/v1")
-
-class SPAStaticFiles(StaticFiles):
-    async def get_response(self, path: str, scope):
-        try:
-            return await super().get_response(path, scope)
-        except (HTTPException, StarletteHTTPException) as ex:
-            if ex.status_code == 404:
-                return await super().get_response("index.html", scope)
-            else:
-                raise ex
-
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))  # project root
-static_path = os.path.join(BASE_DIR, "static")
-# Serve static assets (JS, CSS, images)
-# app.mount("/", SPAStaticFiles(directory="static", html=True), name="whatevs")
-app.mount("/", SPAStaticFiles(directory=static_path, html=True), name="whatevs")
