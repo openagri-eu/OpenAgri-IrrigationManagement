9899ad49b1ac67496269fe7276af4c6da75da66f
Stefan Drobić <stefan.drobic@vizlore.com>
2025-09-08 17:58:59 +0200

    added ETo API that works with parcels from fc and weather data from wd

--- app/api/api_v1/endpoints/eto.py
+++ app/api/api_v1/endpoints/eto.py
@@ -8,9 +8,8 @@ from api import deps
 import crud
 from api.deps import get_jwt
 
-from schemas import EToResponse
-from utils import jsonld_eto_response
-
+from schemas import EToResponse, Calculation
+from utils import jsonld_eto_response, fetch_parcel_by_id, fetch_parcel_lat_lon, TimeUnit, fetch_weather_data
 
 router = APIRouter()
 
@@ -54,4 +53,54 @@ def get_calculations(
         return eto_response
     else:
         jsonld_response = jsonld_eto_response(eto_response)
+        return jsonld_response
+
+
+@router.get("/calculate-gk/")
+def calculate_eto_via_gk(
+        parcel_id: str,
+        from_date: datetime.date,
+        to_date: datetime.date,
+        access_token: str = Depends(get_jwt),
+        db: Session = Depends(deps.get_db),
+        formatting: Literal["JSON", "JSON-LD"] = "JSON"
+):
+    """
+    Returns requested ETo calculations based on FC farm parcel and date interval
+    """
+
+    if from_date > to_date:
+        raise HTTPException(
+            status_code=400,
+            detail="from_date must be later than to_date, from_date: {} | to_date: {}".format(from_date, to_date)
+        )
+
+    parcel_fc = fetch_parcel_by_id(access_token=access_token, parcel_id=parcel_id)
+
+    if not parcel_fc:
+        raise HTTPException(
+            status_code=400,
+            detail="Parcel with ID:{} doesn't exist".format(parcel_id)
+        )
+
+    lat, lon = fetch_parcel_lat_lon(parcel_fc)
+
+    weather_data = fetch_weather_data(
+        latitude=lat, longitude=lon, access_token=access_token, start_date=from_date, end_date=to_date,
+        variables=["et0_fao_evapotranspiration"]
+    )
+
+    response_json = EToResponse(
+        calculations=[
+            Calculation(
+                date=wd["date"],
+                value=wd["values"]["et0_fao_evapotranspiration"]
+            ) for wd in weather_data["data"]
+        ]
+    )
+
+    if formatting.lower() == "json":
+        return response_json
+    else:
+        jsonld_response = jsonld_eto_response(response_json)
         return jsonld_response
--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,3 +2,5 @@ from .custom_schemas import *
 from .jsonld_utils import *
 from .soil_analysis import *
 from .gkutils import *
+from .fcutils import *
+from .wdutil import *
