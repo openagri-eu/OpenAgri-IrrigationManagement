e9ffcc7d9ba132bc159ba51fa6370fed9ee46602
Aleksandar <aleksandar.tomcic@vizlore.com>
2025-10-28 11:05:42 +0100

    new test added

--- tests/tests_.py
+++ tests/tests_.py
@@ -5,6 +5,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'app'))
 
 from fastapi.testclient import TestClient
 
+from schemas import WeightScheme
+
 from app.main import app
 
 client = TestClient(app)
@@ -12,3 +14,16 @@ client = TestClient(app)
 def test_get_all_datasets():
     response = client.get("/dataset")
     assert response.status_code == 200
+
+
+def test_input_weights():
+    response = client.get("dataset/weights")
+    assert response.status_code == 200
+    assert response.json() == {
+        10: 0.15,
+        20: 0.20,
+        30: 0.20,
+        40: 0.15,
+        50: 0.15,
+        60: 0.15,
+    }
